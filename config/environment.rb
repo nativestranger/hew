1a2eb8d1d184e37831ef4b469cedf7d2b8b075de
nativestranger <eric.ed.arnold@gmail.com>
2019-05-19 18:29:37 -0500

    default url options

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,16 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+Rails.application.routes.default_url_options[:host] = Rails.application.secrets.host
+
+if Rails.env.production?
+  ActionMailer::Base.smtp_settings = {
+    address:              'smtp.sendgrid.net',
+    port:                 '587',
+    authentication:       :plain,
+    user_name:            ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_PASSWORD'],
+    domain:               'heroku.com',
+    enable_starttls_auto: true
+  }
+end
