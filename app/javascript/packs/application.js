bae25bcae3a6f2bc3cb91516f718fbc6346d0a23
nativestranger <eric.ed.arnold@gmail.com>
2019-05-20 07:51:09 -0500

    remove stimulus

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,10 +17,3 @@
 
 import 'stylesheets/application'
 import 'bootstrap'
-
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
-
-const application = Application.start()
-const context = require.context("controllers", true, /\.js$/)
-application.load(definitionsFromContext(context))
