a729bd1601e41157bbbed6db97450838a0c226ff
nativestranger <eric.ed.arnold@gmail.com>
2019-08-23 23:11:15 -0500

    dismiss flash

--- app/helpers/bootstrap_flash_helper.rb
+++ app/helpers/bootstrap_flash_helper.rb
@@ -18,7 +18,7 @@ module BootstrapFlashHelper
 
       tag_class = options.extract!(:class)[:class]
       tag_options = {
-        class: "alert fade in alert-#{type} #{tag_class}"
+        class: "alert alert-#{type} #{tag_class} fade show"
       }.merge(options)
 
       close_button = content_tag(:button, raw("&times;"), type: "button", class: "close", "data-dismiss" => "alert")
