4470bacfe35a2097e43f5253dbca9d24985258ac
nativestranger <eric.ed.arnold@gmail.com>
2019-08-24 14:09:29 -0500

    factory / travis

--- spec/factories/shows.rb
+++ spec/factories/shows.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :show do
     name { Faker::Name.name }
     venue { create(:venue) }
-    start_at { Time.current + rand(1..7).days }
-    end_at { start_at + rand(1..3).days }
+    start_at { Time.current + rand(2..7).days }
+    end_at { start_at + rand(8..10).days }
     overview { Faker::Movies::Lebowski.quote }
     full_description { Faker::Lorem.paragraphs(rand(1..3)).join(' ') }
     application_deadline { 1.day.from_now }
