02de9f7bf34c1b1e07b81b1f70d7a223cca8eec0
nativestranger <eric.ed.arnold@gmail.com>
2019-08-23 21:54:07 -0500

    cleanup

--- spec/factories/shows.rb
+++ spec/factories/shows.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :show do
-    name { "MyString" }
+    name { Faker::Name.name }
     venue { nil }
-    start_at { "2019-08-21 22:40:50" }
-    end_at { "2019-08-21 22:40:50" }
+    start_at { Time.current + 7.days }
+    end_at { Time.current + 8.days }
     overview { "MyString" }
     full_desrcription { "MyText" }
-    application_deadline { "2019-08-21 22:40:50" }
+    application_deadline { 1.day.from_now }
     application_details { "MyText" }
   end
 end
