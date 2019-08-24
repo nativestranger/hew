ca2507173233911918ff80eb9cb409ae5bc32bef
nativestranger <eric.ed.arnold@gmail.com>
2019-08-23 22:58:08 -0500

    move fixture images

--- spec/factories/carousel_images.rb
+++ spec/factories/carousel_images.rb
@@ -3,13 +3,13 @@ include ActionDispatch::TestProcess # for seed
 FactoryBot.define do
   factory :carousel_image do
     trait :van do
-      img_upload { fixture_file_upload(Rails.root.join('app', 'assets', 'images', 'van.jpg')) }
+      img_upload { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'images', 'van.jpg')) }
     end
     trait :bathroom do
-      img_upload { fixture_file_upload(Rails.root.join('app', 'assets', 'images', 'bathroom.jpg')) }
+      img_upload { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'images', 'bathroom.jpg')) }
     end
     trait :face do
-      img_upload { fixture_file_upload(Rails.root.join('app', 'assets', 'images', 'face.jpg')) }
+      img_upload { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'images', 'face.jpg')) }
     end
   end
 end
