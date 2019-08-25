af68b5d7bb17e57e3c91f580f2bbaec526248474
nativestranger <eric.ed.arnold@gmail.com>
2019-08-24 23:48:17 -0500

    cleanup

--- spec/factories/show_applications.rb
+++ spec/factories/show_applications.rb
@@ -1,12 +1,3 @@
-# t.bigint "show_id"
-# t.bigint "user_id"
-# t.text "artist_statement", default: "", null: false
-# t.string "artist_website", default: "", null: false
-# t.string "artist_instagram_url", default: "", null: false
-# t.string "photos_url", default: "", null: false
-# t.string "supplemental_material_url", default: "", null: false
-# t.integer "status_id", default: 0, null: false
-
 FactoryBot.define do
   factory :show_application do
     show { create(:show) }
