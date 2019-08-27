7044a75763e30351b625c99a92cd418767f13cb1
nativestranger <eric.ed.arnold@gmail.com>
2019-08-26 23:52:07 -0500

    migration edit

--- db/migrate/20190827042123_create_connections.rb
+++ db/migrate/20190827042123_create_connections.rb
@@ -8,5 +8,12 @@ class CreateConnections < ActiveRecord::Migration[5.2]
 
     add_foreign_key :connections, :users, column: :user1_id
     add_foreign_key :connections, :users, column: :user2_id
+
+    Chat.all.each do |chat|
+      user1 = chat.chatworthy.show.user
+      user2 = chat.chatworthy.user
+      connection = Connection.create!(user1: user1, user2: user2)
+      chat.update!(chatworthy: connection)
+    end
   end
 end
