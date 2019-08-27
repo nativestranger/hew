7f4b99fe3052f45ba4a82862b43e451b70ffa6b7
nativestranger <eric.ed.arnold@gmail.com>
2019-08-26 22:09:00 -0500

    is_admin

---
+++ db/migrate/20190827030631_admin_users.rb
@@ -0,0 +1,9 @@
+class AdminUsers < ActiveRecord::Migration[5.2]
+  def change
+    change_table :users, bulk: true do |t|
+      t.boolean :is_admin, null: false, default: false
+    end
+
+    User.find_by!(email: 'eric.ed.arnold@gmail.com')&.update!(is_admin: true)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_25_061354) do
+ActiveRecord::Schema.define(version: 2019_08_27_030631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
@@ -183,6 +183,7 @@ ActiveRecord::Schema.define(version: 2019_08_25_061354) do
     t.string "artist_website", default: "", null: false
     t.string "instagram_url", default: "", null: false
     t.string "gravatar_url", default: "", null: false
+    t.boolean "is_admin", default: false, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
