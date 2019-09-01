31ce3a83b2af5f38106dbcf60778ceff996df474
nativestranger <eric.ed.arnold@gmail.com>
2019-09-01 17:45:13 -0500

    remove some old stuff

--- db/seed_states_and_cities.rb
+++
@@ -1,9 +0,0 @@
-CSV.foreach("#{Rails.root}/db/data/states/united_states.csv", headers: true) do |csv_row|
-  country = Country.united_states
-  State.where(country: country, name: csv_row.fetch('name')).first_or_create!
-end
-
-CSV.foreach("#{Rails.root}/db/data/states/mexico.csv", headers: true) do |csv_row|
-  country = Country.mexico
-  State.where(country: country, name: csv_row.fetch('name')).first_or_create!
-end
