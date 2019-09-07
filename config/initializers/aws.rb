938082b902eb7d30484597fc3c45040370ab1a35
nativestranger <eric.ed.arnold@gmail.com>
2019-09-07 15:36:14 -0500

    aws

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,8 +1,6 @@
-if Rails.configuration.active_storage.service == :amazon
-  Aws.config.update({
-    region: 'us-east-2',
-    credentials: Aws::Credentials.new(ENV.fetch('AWS_ACCESS_KEY_ID'), ENV.fetch('AWS_SECRET_KEY')),
-  })
+Aws.config.update({
+  region: 'us-east-2',
+  credentials: Aws::Credentials.new(ENV.fetch('AWS_ACCESS_KEY_ID'), ENV.fetch('AWS_SECRET_KEY')),
+})
 
-  S3_BUCKET = Aws::S3::Resource.new.bucket(ENV.fetch('S3_BUCKET_NAME'))
-end
+S3_BUCKET = Aws::S3::Resource.new.bucket(ENV.fetch('S3_BUCKET_NAME'))
