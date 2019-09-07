ce24393410b854e17c53890ada5d05c82ee68435
nativestranger <eric.ed.arnold@gmail.com>
2019-09-07 15:38:15 -0500

    dont fetch

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,6 @@
 Aws.config.update({
   region: 'us-east-2',
-  credentials: Aws::Credentials.new(ENV.fetch('AWS_ACCESS_KEY_ID'), ENV.fetch('AWS_SECRET_KEY')),
+  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_KEY']),
 })
 
-S3_BUCKET = Aws::S3::Resource.new.bucket(ENV.fetch('S3_BUCKET_NAME'))
+S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET_NAME'])
