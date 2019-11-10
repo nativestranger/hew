937d0c09eed7f3ca0b3f5b47d1e81813f6b7c66e
nativestranger <eric.ed.arnold@gmail.com>
2019-11-10 17:45:19 -0500

    cleanup migrations

--- db/migrate/20190829041142_change_addresses.rb
+++ db/migrate/20190829041142_change_addresses.rb
@@ -4,15 +4,6 @@ class ChangeAddresses < ActiveRecord::Migration[5.2]
     add_column :addresses, :state, :string, null: false, default: ''
     add_column :addresses, :country, :string, null: false, default: ''
 
-    Address.all.each do |a|
-      city = City.find(a.city_id)
-      a.update!(
-        city:    city.name,
-        state:   city.state.name,
-        country: city.state.country.name
-      )
-    end
-
     remove_foreign_key :addresses, :cities
     remove_foreign_key :cities, :states
     remove_foreign_key :states, :countries
