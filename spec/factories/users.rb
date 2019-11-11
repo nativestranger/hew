e5e92cafe294ca8534de31ce5ca8f7a8d9de9014
nativestranger <eric.ed.arnold@gmail.com>
2019-11-10 20:24:29 -0500

    style

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -50,6 +50,7 @@ FactoryBot.define do
     email { "#{SecureRandom.uuid[0..5]}@example.com" }
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
+    artist_statement { Faker::Movies::Lebowski.quote }
     artist_website { "https://#{ SecureRandom.uuid[0..5] }.com" }
     instagram_url { "https://#{ SecureRandom.uuid[0..5] }.com" }
     password      { 'password' }
