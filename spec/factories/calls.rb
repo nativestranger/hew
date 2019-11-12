508e590ef1d673b6619882f98f76c6df725467e1
nativestranger <eric.ed.arnold@gmail.com>
2019-11-11 21:52:38 -0500

    update calls factory

--- spec/factories/calls.rb
+++ spec/factories/calls.rb
@@ -41,6 +41,7 @@ FactoryBot.define do
     end_at { start_at + rand(8..10).days }
     overview { Faker::Movies::Lebowski.quote }
     external_url { external ? "https://#{ SecureRandom.uuid[0..5] }.com" : '' }
+    call_type_id { [1,2,3].sample }
     full_description { Faker::Lorem.paragraphs(rand(1..3)).join(' ') }
     application_deadline { start_at - rand(1..7.days) }
     application_details { Faker::Lorem.paragraphs(rand(1..3)).join(' ') }
