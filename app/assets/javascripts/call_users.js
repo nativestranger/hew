850a26259b912a4e1beda4e2e5e1d952180a3846
nativestranger <eric.ed.arnold@gmail.com>
2020-01-03 17:53:48 -0500

    update call users index page

--- app/assets/javascripts/call_users.js
+++ app/assets/javascripts/call_users.js
@@ -1,27 +1,24 @@
 document.addEventListener("turbolinks:load", function() {
   $(document).on('change', '.call_user_role_select', function(event) {
-    let callUserId = $(this).data().callUserId.toString();
-
-    $(this).blur();
+    let roleSelect = this;
+    let callUserId = $(roleSelect).data().callUserId.toString()
+    $(roleSelect).blur();
 
     $.ajax({
       type: 'PUT',
       url: window.location.pathname + '/' + callUserId, // 'calls/x/call_users/y'
       data: {
-        call_user: {
-          role: $(this).val()
-        },
+        call_user: { role: $(roleSelect).val() },
         authenticity_token: App.getMetaContent("csrf-token")
       },
       dataType: 'json',
       success: function(data) {
-        let tdId = '#call_user_' + callUserId + '_role_select_td';
-        $(tdId).removeClass('bg-white');
-        $(tdId).addClass('bg-success');
+        $(roleSelect).removeClass('bg-white');
+        $(roleSelect).addClass('bg-success text-white');
         setTimeout(function() {
-          $(tdId).addClass('bg-white');
-          $(tdId).removeClass('bg-success');
-        }, 200);
+          $(roleSelect).addClass('bg-white');
+          $(roleSelect).removeClass('bg-success text-white');
+        }, 100);
       },
       error: function(data) {
         alert('Something went wrong. Please contact us if this persists.');
