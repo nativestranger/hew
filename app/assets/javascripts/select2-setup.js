54d8f69f75a4a8e17ae87c3a789de94d1d1599b8
nativestranger <eric.ed.arnold@gmail.com>
2020-01-08 22:13:50 -0500

    dont allow all digit categories

--- app/assets/javascripts/select2-setup.js
+++ app/assets/javascripts/select2-setup.js
@@ -1,6 +1,19 @@
 document.addEventListener("turbolinks:load", function() {
   $( "#call_category_ids" ).select2({
     tags: true,
-    theme: "bootstrap"
+    theme: "bootstrap",
+    createTag: function (params) {
+      var term = $.trim(params.term);
+
+      if (/^\s?\d+?\s?$/.test(term)) {
+        return null;
+      }
+
+      return {
+        id: term,
+        text: term,
+        newTag: true // add additional parameters
+      }
+    }
   });
 });
