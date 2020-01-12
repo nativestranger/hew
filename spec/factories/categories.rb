57edbb8fcd8db2d9470e96d21354605396ff5a74
nativestranger <eric.ed.arnold@gmail.com>
2020-01-11 20:51:49 -0500

    cleanup

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id         :bigint           not null, primary key
+#  name       :string           default(""), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_categories_on_name  (name) UNIQUE
+#
+
 FactoryBot.define do
   factory :category do
     name { Faker::Name.unique.name }
