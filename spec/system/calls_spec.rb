0677cc32d902ccfec9d3946df8b4e7420e55cb87
nativestranger <eric.ed.arnold@gmail.com>
2020-01-15 21:36:01 -0500

    fix spec

--- spec/system/calls_spec.rb
+++ spec/system/calls_spec.rb
@@ -39,13 +39,16 @@ RSpec.describe 'Calls', type: :system do
     fill_in 'call_overview', with: 'Call overview'
 
     find('.call_application_deadline').click
+    find('.react-datepicker__navigation--next').click
     all(".react-datepicker__day").find { |day| day.text == "3" }.click
     all(".react-datepicker__time-list-item").find { |day| day.text == "12:00 AM" }.click
 
     find('.call_start_at').click
+    find('.react-datepicker__navigation--next').click
     all(".react-datepicker__day").find { |day| day.text == "4" }.click
 
     find('.call_end_at').click
+    find('.react-datepicker__navigation--next').click
     all(".react-datepicker__day").find { |day| day.text == "5" }.click
 
     page.execute_script("document.getElementById('call_full_description').value = 'desc'")
@@ -155,6 +158,7 @@ RSpec.describe 'Calls', type: :system do
       page.all(:xpath, "//span[@class='select2-selection__choice__remove']").first.click
 
       click_button 'Save'
+
       expect(call.call_users.find_by!(user: juror).reload.categories).to be_empty
       expect(call.reload.categories).to be_empty
     end
