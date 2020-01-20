d359b791b48125f9d92b39400a1a95fcef32a0f9
nativestranger <eric.ed.arnold@gmail.com>
2020-01-19 23:51:00 -0500

    cafe job

--- app/jobs/call_for_entry_job.rb
+++ app/jobs/call_for_entry_job.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class CallForEntryJob < ActiveJob::Base
-  # TODO: individual one
+  def perform(call_id)
+    ENV['call_id'] = call_id.to_s # TODO: not this
+
+    CafeSpider.crawl!
+  end
 end
