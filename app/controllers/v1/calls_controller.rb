662b14809b3a431be845693e1e89fa3077a6a904
nativestranger <eric.ed.arnold@gmail.com>
2020-01-25 15:57:38 -0500

    cleanup

--- app/controllers/v1/calls_controller.rb
+++ app/controllers/v1/calls_controller.rb
@@ -4,6 +4,7 @@ class V1::CallsController < V1Controller
   def index
     @calls = CallSearcher.new(
       call_name: params[:name],
+      call_type_ids: params[:call_type_ids],
       order_option: params[:order_option],
       user: current_user
     ).records
